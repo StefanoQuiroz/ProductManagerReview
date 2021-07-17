e0ed4aa2f671f6a04d1971a651e16defa73236eb
Stefano <piero.quiroz93@gmail.com>
2021-07-17 18:44:51 -0500

    Views added Main

---
+++ client/src/views/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import ProductoForm from '../components/ProductoForm';
+
+const Main = () => {
+    return (
+        <Container>
+            <ProductoForm/>            
+        </Container>
+    );
+}
+
+export default Main;
