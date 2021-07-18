8871a107a96afd25799b2c4a2f3f642300bbf9e0
Stefano <piero.quiroz93@gmail.com>
2021-07-17 23:57:14 -0500

    Added new Context for components

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
 import ProductoForm from '../components/ProductoForm';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ProductLits from '../components/ProductLits';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaPlus } from 'react-icons/fa';
+
+export const MyContext = createContext(); 
 
 const Main = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/product")
+            .then(response => setProducts(response.data.datos))
+            .catch(err => Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text : 'Ha ocurrido un error al obtener los datos'
+            }))
+    },[])
     return (
         <Container>
-            <Router>
-                <Switch>
-                    <Route path={`/`}>
-                        <ProductoForm/>            
-                    </Route>
-                </Switch>
-            </Router>
+            <MyContext.Provider value={{products, setProducts}}>
+                <Router>
+                    <Link to={`/create/new`}>
+                        <FaPlus style={{margin:'2rem'}}/>
+                    </Link>
+                    <Switch>
+                        <Route path={`/create/new`}>
+                            <ProductoForm/>            
+                        </Route>
+                        <Route path={`/`}>
+                            <ProductLits/>            
+                        </Route>
+                    </Switch>
+                </Router>
+            </MyContext.Provider>
         </Container>
     );
 }
