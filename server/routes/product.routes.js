f4079960414e6745259a69e2b5f5c19311e36e3d
Stefano <piero.quiroz93@gmail.com>
2021-07-19 18:00:09 -0500

    User actions

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express();
 const {findProduct, findOneProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controllers');
+const { authenticate } = require('../config/jwt.config');
 
-router.get(`/product`, findProduct);
-router.get(`/product/:id`, findOneProduct);
-router.post(`/product/create`, createProduct);
-router.put(`/product/update/:id`, updateProduct);
-router.delete(`/product/delete/:id`, deleteProduct);
+router.get(`/product`, authenticate, findProduct);
+router.get(`/product/:id`, authenticate, findOneProduct);
+router.post(`/product/create`, authenticate, createProduct);
+router.put(`/product/update/:id`, authenticate, updateProduct);
+router.delete(`/product/delete/:id`, authenticate, deleteProduct);
 
 module.exports = router;
 
--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
-
+const cookieParser = require('cookie-parser');
 //Connect to DB
 const connectDB = require('./config/mongoDB');
 connectDB();
 
+app.use(cookieParser());
 //middleware with 2 servers
 app.use(cors({creadentials: true, origin: 'http://localhost:3000'}));
 
@@ -15,7 +16,8 @@ app.use(cors({creadentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(`/api`, require('./routes/product.routes'))
+app.use(`/api`, require('./routes/product.routes'));
+app.use(`/api`, require('./routes/users.routes'));
 
 app.listen(PORT, ()=>{
     console.log(`1 : Server Locked and Loaded at PORT: ${PORT}`)
